382b1869dda1c5212d478116caa209204b8835f5
Emma (UCloud) <emmarisgaard98@gmail.com>
2023-10-10 20:00:21 +0200

    new setup

---
+++ new_setup.sh
@@ -0,0 +1,16 @@
+# run using 
+# bash path_to_folder/setup_git.sh
+
+git config --global user.email "emmarisgaard98@gmail.com"
+git config --global user.name "Emma (UCloud)"
+
+# install virtual environment 
+sudo apt-get update
+sudo apt-get install python3-venv
+
+# activate environment 
+source /work/NLP-AU/nlp-e23/bin/activate
+
+# creating kernel 
+pip install ipykernel
+python3 -m ipykernel install --user --name=nlp-e23
